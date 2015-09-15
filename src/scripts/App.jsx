7aebaac5735983e2caab1916a0e0c8cd9049c3ff
Joe <josephdburdick@gmail.com>
2015-09-15 18:13:28 -0400

    Apply yield to body. Hello, World

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,28 +1,14 @@
-// React.render(
-//   <aboveHorizontalScroller />,
-//   document.querySelector('#overhor-scroll-example')
-// );
-
-  var HelloWorld = React.createClass({
+  var Body = React.createClass({ displayName: "Body",
     componentWillMount(){
-      this.setState({name: ''});
+
     },
-    onChange(event){
-      this.setState({name: event.currentTarget.value});
-    },
-    render() {
-      return (
-        <div>
-          <input type="text" onChange={this.onChange} placeholder="Your name here" />
-          <p>Hello, {this.state.name}! It is {this.props.date.toTimeString()}</p>
-        </div>
+    render(){
+      return(
+        <div>Hello World</div>
       );
     }
   });
 
-  setInterval(function() {
-    React.render(
-      <HelloWorld date={new Date()} />,
-      document.querySelector('#overhor-scroll-example')
-    );
-  }, 500);
+  React.render(
+    <Body />, document.querySelector("#yield")
+  );

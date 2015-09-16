5c91b5f0e7adacd066b9634aea0c469cdd845882
Joe <josephdburdick@gmail.com>
2015-09-16 15:41:31 -0400

    Remove double quote, stop trying to import local JS (for now).

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,19 +1,17 @@
-  import * as React from "../../bower_components/react/react.min.js";
-
-  var Body = React.createClass({ displayName: "Body",
+ var Body = React.createClass({ displayName: 'Body',
     componentWillMount(){ },
     componentDidMount(){ },
     componentWillReceiveProps(){ },
-    componentDidUpdate(prevProps, prevState){ },
+    componentDidUpdate(){ },
     componentWillUnmount(){ },
     render(){
       let route = window.location.hash;
-      return(
-        <div>The requested route is: ${route}</div>
+      return (
+        <div>The requested route is: {route}</div>
       );
     }
   });
 
   React.render(
-    <Body />, document.querySelector("#yield")
+    <Body />, document.querySelector('#yield')
   );

c43a76f03f72f689574bfc46ea12a495ccd10ac7
Joe <josephdburdick@gmail.com>
2015-09-17 18:22:25 -0400

    Remove double negation.

--- src/scripts/helpers/heightRatio.jsx
+++ src/scripts/helpers/heightRatio.jsx
@@ -5,11 +5,11 @@ module.exports = (App) => {
   let heightRatio = () =>{
     $(window).on('resize', () =>{
       $('[data-heightRatio]').each(function(){
-        let ratio = !!$(this).attr('data-heightRatio') ? parseFloat($(this).attr('data-heightRatio')) : config.defaults.heightRatio;
+        let ratio = $(this).attr('data-heightRatio') ? parseFloat($(this).attr('data-heightRatio')) : config.defaults.heightRatio;
         $(this).css({'min-height': $(this).outerWidth() * ratio });
       });
     }).trigger('resize');
   };
-  
+
   return heightRatio;
 };

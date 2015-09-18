d766a2d5cef213377627c1681bb097c37c7ceb82
Joe <josephdburdick@gmail.com>
2015-09-18 10:54:18 -0400

    Update 2015-09-18T14:54:16.671Z

---
+++ scripts/config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = function () {
+  var config = {
+    defaults: {},
+    "private": {}
+  };
+
+  // Defaults
+  config.defaults.heightRatio = 0.55;
+
+  return {
+    defaults: config.defaults
+  };
+};
+//# sourceMappingURL=config.js.map

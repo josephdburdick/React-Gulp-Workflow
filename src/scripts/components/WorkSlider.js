3ec4700afde07f2cfa925e12038b7eb45186e1cd
Joe <josephdburdick@gmail.com>
2015-10-14 15:02:33 -0400

    Add responsive features to carousel.

--- src/scripts/components/WorkSlider.js
+++ src/scripts/components/WorkSlider.js
@@ -8,7 +8,35 @@ let
         infinite: false,
         speed: 500,
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: false,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       };
       return (
         <Slider {...settings}>
